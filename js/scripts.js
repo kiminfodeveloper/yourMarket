2c4a13293e205de2d65cce8732adbebd1df83123
Kevyn Melo <kimdeveloper@hotmail.com>
2024-03-26 17:08:53 -0300

    new features
    overflows and faqs

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,49 @@
 const textElement = document.getElementById("transition-text");
-const texts = ["Bem vindo(a) ao Your Market.", "Acesse nosso Whatsapp abaixo.", "O mercado ao lado do seu lar.", "Your Market, o mercado autônomo."];
+const texts = [
+    "Bem vindo(a) ao Your Market.",
+    "Acesse nosso Whatsapp abaixo.",
+    "24h por dia, venha conhecer.",
+    "Your Market, o mercado do seu lar.",
+    "Seu condomínio pode ser o próximo.",
+    "Tenha tudo a um elevador de distância.",
+    "Quer conhecer mais? Clique abaixo",
+];
 let currentTextIndex = 0;
 
 function changeText() {
-  // Pega o texto atual
-  const currentText = texts[currentTextIndex];
+    // change actual text
+    const currentText = texts[currentTextIndex];
 
-  // Define o novo texto
-  textElement.textContent = currentText;
+    // new txt changing
+    textElement.textContent = currentText;
 
-  // Aumenta o índice do texto
-  currentTextIndex = (currentTextIndex + 1) % texts.length;
+    // up ind array
+    currentTextIndex = (currentTextIndex + 1) % texts.length;
 
-  // Chama a função novamente após um intervalo
-  setTimeout(changeText, 2000);
+    // get function after timer
+    setTimeout(changeText, 2000);
 }
 
-// Inicia a mudança de texto
+//start change text
 changeText();
 
-const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector(".back-to-top");
 
 const backToTop = () => {
- if (window.scrollY >= 100) {
-backToTopButton.classList.add('show')
- } else {
-backToTopButton.classList.remove('show')
- }
-}
+    if (window.scrollY >= 100) {
+        backToTopButton.classList.add("show");
+    } else {
+        backToTopButton.classList.remove("show");
+    }
+};
 function scrollToTop() {
-    // função de voltar ao topo
+    // back to top fucntion
     window.scrollTo({
-      top: 0,
-      behavior: "smooth"
+        top: 0,
+        behavior: "smooth",
     });
-  }
+}
 
-window.addEventListener('scroll', function () {
- backToTop()
-})
+window.addEventListener("scroll", function () {
+    backToTop();
+});
